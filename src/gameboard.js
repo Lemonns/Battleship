99d282d6d8c9ac0fc3ca1d4415c98a185c5ffeed
Trey <treymarlette2000@gmail.com>
2022-09-29 18:40:18 -0500

    Gameboard updated

--- src/gameboard.js
+++ src/gameboard.js
@@ -3,6 +3,8 @@ import { Ship } from "./ship";
 const SIZE = 10;
 export const Gameboard = () => {
     let board = [];
+    let missedShots = [];
+    let sunk = false;
 
     const initializeBoard = (() => {
         for (let i = 0; i < SIZE; i++) {
@@ -37,11 +39,13 @@ export const Gameboard = () => {
     //STORE OBJECTS IN BOARD ARRAY INSTEAD OF "ship"
     const placeShip = (loc1, loc2, size, direction) => {
         let ship = Ship(size);
+        let shipPos = 0
         if (direction === "horizontal") {
             if ((loc2 + ship.shipSize) > 10) return false;
             if (canPlace(loc1, loc2, size, direction)) {
                 for (let i = 0; i < ship.shipSize; i++) {
-                    board[loc1][loc2+i] = "ship";
+                    board[loc1][loc2+i] = {ship, shipPos};
+                    shipPos++
                 }
             }
         }
@@ -50,7 +54,8 @@ export const Gameboard = () => {
             if ((loc1 + ship.shipSize) > 10) return false;
             if (canPlace(loc1, loc2, size, direction)) {
                 for (let i = 0; i < ship.shipSize; i++) {
-                    board[loc1+i][loc2] = "ship";
+                    board[loc1+i][loc2] = {ship, shipPos};
+                    shipPos++
                 }
             }
 
@@ -58,20 +63,24 @@ export const Gameboard = () => {
         
     }
 
-    //STORE OBJECTS IN BOARD ARRAY INSTEAD OF "ship"
+    
     const receiveAttack = (loc1, loc2) => {
-        if (board[loc1][loc2] === "ship") {
-            //send hit function to ship that was hit
-        }
         if (board[loc1][loc2] === null) {
-            //attack misses
-        }else {
-            //already used attack on this location
+            console.log("Miss")
+            //attack misses; record location missed somehow
+            missedShots.push([loc1, loc2])
+            return board[loc1][loc2]
+        }
+        if (typeof board[loc1][loc2] === 'object' && board[loc1][loc2].ship.shipData[board[loc1][loc2].shipPos] === "hit") {
+            console.log("Already hit")
+            //Don't allow user to click here bc they already attacked at this location
+            return false
+        }
+        if (typeof board[loc1][loc2] === 'object') {
+            board[loc1][loc2].ship.hit(board[loc1][loc2].shipPos)
+            return
         }
     }
-
-
-
     return {board, placeShip, canPlace, receiveAttack};
 }
 
@@ -81,4 +90,7 @@ bor.placeShip(0, 4, 3, "horizontal")
 console.log(bor.board)
 bor.placeShip(1, 0, 4, "horizontal")
 bor.placeShip(5, 1, 4, "vertical")
-//console.log(bor.canPlace(0, 2, 3))
+bor.receiveAttack(4, 5)
+bor.receiveAttack(0, 5)
+bor.receiveAttack(5, 1)
+bor.receiveAttack(5, 1)
--- src/ship.js
+++ src/ship.js
@@ -1,21 +1,23 @@
+import { isInteger } from "lodash";
+
 export const Ship = (size) => {
 
     //Length of array/ship
     let shipSize = size; 
 
     //Array for ship based off of size. False = not hit, True = hit
-    let shipData = new Array(shipSize).fill(false);
+    let shipData = [...Array(shipSize).keys()];
 
    
     //If ship is hit, make index true
     const hit = (loc) => {
-        shipData[loc] = true;
+        shipData[loc] = "hit";
     }
 
     //Checks to see if ship is sunk; If all elements in array are true, it is sunk
     const isSunk = () => {
         const status = shipData.every(ele => {
-            if (ele === shipData[0] && ele != false) return true;
+            if (ele === shipData[0] && isInteger(ele) === false) return true;
             return false;
         })
         return status;
